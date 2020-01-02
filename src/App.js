cdf5290fd06de4aeb66db7129b04e90bee50e861
MasterKN <masterkn48@gmail.com>
2020-01-02 18:42:50 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import React, { useState, useEffect, useRef } from "react";
+
+const App = () => {
+  return (
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Hello World</h1>
+        <p class="subtitle">
+          This is template with <strong>Bulma</strong>!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default App;
