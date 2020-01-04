53f4d2c00f2edebd9519aa3ec342fa44d3d4cba6
MasterKN <masterkn48@gmail.com>
2020-01-04 14:14:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aio/precache-manifest.4d939b2296b322e713f757c1ad09bba8.js"
+  "/aio/precache-manifest.43d20bf04e52e2d250d8fa45b0e99376.js"
 );
 
 self.addEventListener('message', (event) => {
